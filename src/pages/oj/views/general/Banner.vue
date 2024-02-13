622ed58b9d384115583a4146fed33d87a9c9d5e6
Deng Junhai <DengJunHai@foxmail.com>
2024-02-13 14:00:00 +0800

    Add scrolling image
    Add scrolling image

---
+++ src/pages/oj/views/general/Banner.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="block">
+      <Carousel
+        autoplay
+        autoplay-speed="8000"
+        dots="inside"
+        trigger="hover"
+        arrow="hover">
+        <Carousel-item v-for="item in bannerList" :key="item">
+          <div class="banner-item" :style="{ 'background-image': 'url(' + item.imgUrl + ')' }"></div>
+        </Carousel-item>
+      </Carousel>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Banner',
+    components: {},
+    data () {
+      return {
+        bannerList: [
+          {
+            id: 1,
+            imgUrl: 'https://lightxi.com/lightoj/115775949_p0.jpg'
+          },
+          {
+            id: 2,
+            imgUrl: 'https://lightxi.com/lightoj/84050163_p0.jpg'
+          }
+        ]
+      }
+    },
+    mounted () {
+  
+    },
+    methods: {
+  
+    },
+    computed: {
+  
+    }
+  }
+  </script>
+  
+  <style scoped lang="less">
+    .banner-item {
+      margin: 0;
+      height: 400px;
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  
+  </style>
